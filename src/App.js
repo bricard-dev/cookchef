f4ab3f54e8c123b4ad2b50888fca671dc128df08
Bastien Ricard <bastien.ricard-dev@outlook.com>
2023-02-03 17:57:16 +0100

    feat: add application layout
    Add a layout through several components.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import styles from "./App.module.scss";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Coockchef</h1>
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,5 @@
+function Content() {
+  return <h1 className="flex-fill mb-3">Content</h1>;
+}
+
+export default Content;
