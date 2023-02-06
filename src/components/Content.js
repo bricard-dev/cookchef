415db86979b0d8d884ffa43de9f3098e0e8d592a
Bastien Ricard <bastien.ricard-dev@outlook.com>
2023-02-06 16:44:50 +0100

    feat: add recipe like and display fake recipes
    Add the possibility to like a recipe. Replace the previous way to display recipes to display a list of different fake recipes.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,6 @@
 import styles from "./Content.module.scss";
 import Recipe from "./Recipe";
+import { data } from "../data/recipe";
 
 function Content() {
   return (
@@ -7,16 +8,9 @@ function Content() {
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
       <div className={`card p-20 ${styles.contentCard}`}>
         <div className={styles.grid}>
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
+          {data.map((recipe) => (
+            <Recipe key={recipe.id} title={recipe.title} image={recipe.image} />
+          ))}
         </div>
       </div>
     </div>
